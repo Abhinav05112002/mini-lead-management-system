06a9dc5e0d1aacac01c1d458c39b2a7ae84efaab
Abhinav05112002 <abhinavrai044@gmail.com>
2026-06-21 16:43:29 +0530

    Update activity.routes.js

--- backend/src/routes/activity.routes.js
+++ backend/src/routes/activity.routes.js
@@ -1,11 +1,21 @@
+const express = require("express");
+
+const router = express.Router();
+
+const auth =
+require("../middleware/auth.middleware");
+
+const authorize =
+require("../middleware/role.middleware");
+
+const activityController =
+require("../controllers/activity.controller");
+
 router.get(
- "/",
- auth,
- authorize(
-   "ADMIN",
-   "MANAGER"
- ),
- activityController.getLogs
+  "/",
+  auth,
+  authorize("ADMIN", "MANAGER"),
+  activityController.getLogs
 );
 
 module.exports = router;
